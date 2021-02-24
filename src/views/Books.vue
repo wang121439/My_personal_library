b8840aa32adbbb8ac442f8984a8ecc0feb0ef55d
wang <1135889390@qq.com>
2021-02-24 22:54:54 +0800

    第三次提交books

--- src/views/Books.vue
+++ src/views/Books.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
     <!-- 顶部导航开始 -->
-    <mt-header class="Header" style="height:52px;" fixed>
+    <mt-header class="Header" style="height: 52px" fixed>
       <div slot="left">
         <img
           src="../assets/img/books_logo.png"
           alt="懒人书柜"
-          style="width: 85px; display: inherit;"
+          style="width: 85px; display: inherit"
           slot="icon"
         />
       </div>
@@ -32,85 +32,131 @@
           <div>
             <van-swipe
               :autoplay="4000"
-              style="height: 140px;position: relative;top: 52px;">
+              style="height: 140px; position: relative; top: 52px"
+            >
               <van-swipe-item v-for="(item, i) of booksDeta" :key="i">
-                <img v-lazy="item.body_imgb" style="height:140px" />
+                <img v-lazy="item.body_imgb" style="height: 140px" />
               </van-swipe-item>
             </van-swipe>
           </div>
           <!-- 轮播图 -->
-<!-- 导航 -->
-          <div class="booksRow" >
-  <van-row type="flex" justify="space-around" >
-  <van-col span="4">
-    <router-link to="#"> 
-    <img src='../assets/img/page/books_active.png' alt='畅销榜'/>
-    <b>畅销榜</b> 
-   </router-link>
-    </van-col>
-  <van-col span="4">
-    <router-link to="#">
-    <img src='../assets/img/page/books_weed.png'  alt='淘书团'/>
-    <b>淘书团</b></router-link></van-col>
-  <van-col span="4">
-    <router-link to="#">
-    <img src='../assets/img/page/books_mail.png' alt='9.9包邮'/>
-    <b >9.9包邮</b></router-link></van-col>
-  <van-col span="4">
-    <router-link to="#">
-    <img src='../assets/img/page/books_new.png' alt='新上架'/>
-    <b>新上架</b></router-link></van-col>
-  <van-col span="4">
-    <router-link to="#">
-    <img src='../assets/img/page/books_publish.png'  alt='出版社'/>
-    <b>出版社</b></router-link></van-col>
-</van-row> </div>
-<!-- 导航 -->
+          <!-- 导航 -->
+          <div class="booksRow">
+            <van-row type="flex" justify="space-around">
+              <van-col span="4">
+                <router-link to="#">
+                  <img src="../assets/img/page/books_active.png" alt="畅销榜" />
+                  <b>畅销榜</b>
+                </router-link>
+              </van-col>
+              <van-col span="4">
+                <router-link to="#">
+                  <img src="../assets/img/page/books_weed.png" alt="淘书团" />
+                  <b>淘书团</b></router-link
+                ></van-col
+              >
+              <van-col span="4">
+                <router-link to="#">
+                  <img src="../assets/img/page/books_mail.png" alt="9.9包邮" />
+                  <b>9.9包邮</b></router-link
+                ></van-col
+              >
+              <van-col span="4">
+                <router-link to="#">
+                  <img src="../assets/img/page/books_new.png" alt="新上架" />
+                  <b>新上架</b></router-link
+                ></van-col
+              >
+              <van-col span="4">
+                <router-link to="#">
+                  <img
+                    src="../assets/img/page/books_publish.png"
+                    alt="出版社"
+                  />
+                  <b>出版社</b></router-link
+                ></van-col
+              >
+            </van-row>
+          </div>
+          <!-- 导航 -->
 
-<!-- 资讯内容 -->   
-<div style="height: 80px; position:relative; top:38px;margin-top: 5px;">
-<van-row>
-  <van-col span="6">
-    <img src="../assets/img/books_message.png" alt="" style="width:60px;border-radius: 5px;margin: 8px 6px;">
-  </van-col>
-  <van-col span="18">
-    <van-notice-bar left-icon="volume-o" :scrollable="false" style="color:red;background-color:#FFF;right: 24px;">
-<van-swipe
-  vertical
-  class="notice-swipe"
-  :autoplay="3000"
-  :show-indicators="false">
-  <van-swipe-item class="ellipsis"> <router-link to="#">辛丑年新春书市|每满79减30（附送手帐本1个）</router-link> </van-swipe-item>
-  <van-swipe-item class="ellipsis"> <router-link to="#">疫情期间发货公告</router-link></van-swipe-item>
-  <van-swipe-item class="ellipsis"> <router-link to="#">新用户注册即送10元!</router-link></van-swipe-item>
-</van-swipe>
-</van-notice-bar>
-  </van-col>
-</van-row>
-<!-- 资讯内容 -->
-</div>
-<!-- 广告轮播 -->
-<div  > 
-<van-swipe :autoplay="3000" style="position: initial;" id="indicators" >
-  <van-swipe-item v-for="(image, index) in  books_images" :key="index">
-     <router-link to=""><img v-lazy="image" style="width:100%;" /></router-link>
-  </van-swipe-item>
-</van-swipe>
- </div>
-<!-- 广告轮播 -->
+          <!-- 资讯内容 -->
+          <div
+            style="height: 80px; position: relative; top: 38px; margin-top: 5px"
+          >
+            <van-row>
+              <van-col span="6">
+                <img
+                  src="../assets/img/books_message.png"
+                  alt=""
+                  style="width: 60px; border-radius: 5px; margin: 8px 6px"
+                />
+              </van-col>
+              <van-col span="18">
+                <van-notice-bar
+                  left-icon="volume-o"
+                  :scrollable="false"
+                  style="color: red; background-color: #fff; right: 24px"
+                >
+                  <van-swipe
+                    vertical
+                    class="notice-swipe"
+                    :autoplay="3000"
+                    :show-indicators="false"
+                  >
+                    <van-swipe-item class="ellipsis">
+                      <router-link to="#"
+                        >辛丑年新春书市|每满79减30（附送手帐本1个）</router-link
+                      >
+                    </van-swipe-item>
+                    <van-swipe-item class="ellipsis">
+                      <router-link to="#"
+                        >疫情期间发货公告</router-link
+                      ></van-swipe-item
+                    >
+                    <van-swipe-item class="ellipsis">
+                      <router-link to="#"
+                        >新用户注册即送10元!</router-link
+                      ></van-swipe-item
+                    >
+                  </van-swipe>
+                </van-notice-bar>
+              </van-col>
+            </van-row>
+            <!-- 资讯内容 -->
+          </div>
+          <!-- 广告轮播 -->
+          <div>
+            <van-swipe
+              :autoplay="3000"
+              style="position: initial"
+              id="indicators">
+              <van-swipe-item
+                v-for="(image, index) in books_images"
+                :key="index">
+                <router-link to=""><img v-lazy="image" style="width: 100%"
+                /></router-link>
+              </van-swipe-item>
+            </van-swipe>
+          </div>
+          <!-- 广告轮播 -->
         </mt-tab-container-item>
       </mt-tab-container>
-      <div  class="books_subhead">
- <span style="margin-left:10px;">今日值得买</span> 
-  <router-link to="">更多»</router-link>
-</div>
+      <div class="books_subhead">
+        <span style="margin-left: 10px">今日值得买</span>
+        <router-link to="">更多»</router-link>
+      </div>
+      <div>
 
-<div>
+<van-grid :border="false" :column-num="3">
+  <van-grid-item  v-for="(item,i) of booksimg" :key='i' >
+    <van-image :src=item.img_imgs style="width:100%" />
+  <van-grid-item>{{}}</van-grid-item>
+  </van-grid-item>
+</van-grid>
 
 
-
-</div>
-      
+      </div>
     </div>
     <!-- 面板区域结束 -->
 
@@ -201,38 +247,67 @@
 import { Swipe, SwipeItem } from "mint-ui";
 import "mint-ui/lib/style.css";
 
+
 export default {
   name: "Swiper",
   data() {
     return {
       booksTab: "home",
       booksDeta: [],
-      books_images:[
-        'http://image31.bookschina.com/pro-images/spic/fzp700185.jpg',
-        'http://image31.bookschina.com/pro-images/201230bg/700185.jpg?=2',
-        'http://image31.bookschina.com/pro-images/201230wz/700185.jpg?=2'
+      books_images: [
+        "http://image31.bookschina.com/pro-images/spic/fzp700185.jpg",
+        "http://image31.bookschina.com/pro-images/201230bg/700185.jpg?=2",
+        "http://image31.bookschina.com/pro-images/201230wz/700185.jpg?=2",
       ],
-      booksimg:[]
-     
+      booksimg: [],
+      
     };
   },
 
   components: {
     "mt-swipe": Swipe,
-    "mt-swipe-item": SwipeItem
+    "mt-swipe-item": SwipeItem,
   },
   computed: {},
 
-  methods: {},
+  methods: {
+
+
+  },
+
+
   mounted() {
-    this.axios.get("/body").then(res => {
+
+    
+    this.axios.get("/body").then((res) => {
       let results = res.data.results;
       this.booksDeta = results;
     });
-     this.axios.get("/img").then(res => {
-let results = res.data.results;
-this.booksimg= results;})
-  }
+    this.axios.get("/img").then((res) => {
+      let results = res.data.results;
+      this.booksimg = results;
+      console.log(this.booksimg)
+  let [{img_subhead},{img_imgs},{img_pric}]=this.booksimg;
+  let img_subhead=[];
+
+for (let index = 0; index < img_subhead.length; index++) {
+  let subhead=img_subhead;
+ console.log(img_subhead);
+  let [bh,bb]=subhead.split('/');
+  console.log(bh);
+  //  console.log(bb);
+
+  
+}
+
+
+ 
+ 
+
+
+
+    });
+  },
 };
 </script>
 <style>
@@ -305,73 +380,65 @@ this.booksimg= results;})
   opacity: 1;
 }
 
-
-.van-swipe{
+.van-swipe {
   top: 95px;
 }
 
-
 /* 导航 */
-.booksRow{
-  height:90px;
+.booksRow {
+  height: 90px;
   position: relative;
-  top: 52px; 
+  top: 52px;
   padding: 0px 12px;
   margin-top: 10px;
 }
 
-.booksRow img{
-width:48px;
-padding-bottom:2px;
-
+.booksRow img {
+  width: 48px;
+  padding-bottom: 2px;
 }
-.booksRow  b{
-  font-size:0.9rem;
-  margin-left:4px;
+.booksRow b {
+  font-size: 0.9rem;
+  margin-left: 4px;
   color: #000;
 }
 /* 导航 */
 
 /* 资讯 */
 
-.ellipsis{
-color: #000;
-overflow: hidden;
-text-overflow: ellipsis;
-white-space: nowrap;
-
+.ellipsis {
+  color: #000;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
 }
-.notice-swipe  a{
-color:#000;
+.notice-swipe a {
+  color: #000;
 }
 /* 资讯 */
 
 /* 副标题 */
-.books_subhead{
-  height:40px;
-   display: flex;
-   justify-content: space-between;
-   align-items: center;
-   background-color: #F9BD00;
-   margin-top: 8px;
+.books_subhead {
+  height: 40px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #f9bd00;
+  margin-top: 8px;
 }
-.books_subhead span,a{
-  color: #FFF;
+.books_subhead span,
+a {
+  color: #fff;
 }
 
-.books_subhead a{
-margin-right:10px;
-border: 1px solid #fff;
-border-radius: 0.5rem;
- padding: 5px 10px;
- font-size: 0.5rem;
- line-height: 0.28rem;
- overflow: hidden;
-
+.books_subhead a {
+  margin-right: 10px;
+  border: 1px solid #fff;
+  border-radius: 0.5rem;
+  padding: 5px 10px;
+  font-size: 0.5rem;
+  line-height: 0.28rem;
+  overflow: hidden;
 }
 /* 副标题 */
-
-
-
-
 </style>
